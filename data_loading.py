101e3ced28831b84ca08e2bcc9cf5fcb13baa6bf
sanabria <sanabria.renato@bcg.com>
2018-09-21 09:13:55 -0300

    interface for data loading

--- data_loading.py
+++ data_loading.py
@@ -1,7 +1,14 @@
 import pandas as pd 
 
 
-def load(path = './IONIZATION_case_daily_production_data.xlsx'):
-	
+def load(path):
+	"""
+	Reads historical production data based on file path in 
+	config file
+
+	@retuns: df with 9 columns:
+	[day, month, year, week_number,slab_production,pickeld_production,
+	galvanized_1_production, galvanized_2_production,galvanized_3_production]
+	"""
 	df = pd.read_excel(path)
 	return df
