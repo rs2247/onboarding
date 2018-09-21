29482bcc10ce901c1a2b4e8b29c879da600a77a7
MartinM4 <mtin@gmx.net>
2018-09-21 14:13:37 +0200

    added settings

--- data_loading.py
+++ data_loading.py
@@ -1,7 +1,11 @@
 import pandas as pd 
 
 
-def load(path = './IONIZATION_case_daily_production_data.xlsx'):
-	
+def load(path: str = './IONIZATION_case_daily_production_data.xlsx') -> pd.DataFrame:
+	"""
+	Reading the data from file
+	:param path: path to read from
+	:return: DataFrame
+	"""
 	df = pd.read_excel(path)
 	return df
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import pandas as pd 
+import pandas as pd
+import json
+
 import data_loading
 import model
 
+#constants for reading settings
+PATH = 'path'
+
+def read_settings() -> dict:
+	with open('config.json') as f:
+		settings = json.load(f)
+	return settings
 
 if __name__ == '__main__':
-
-	data = data_loading.load()
+	settings = read_settings()
+	data = data_loading.load(settings[PATH])
 	model.capacity_prediction(data)
